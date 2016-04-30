696d029d89d58720257eb22f59a9b65ec5a1c5e5
awdesh <awdesh.sharma1@gmail.com>
2016-04-29 22:17:33 -0700

    adding test file

---
+++ test/bot.spec.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const assert = require('assert');
+const chai = require('chai');
+chai.should();
+chai.use(require('chai-as-promised'));
+const expect = chai.expect;
+const hat = require('hat');
+const smoochBot = require('smooch-bot');
+const MemoryStore = smoochBot.MemoryStore;
+const MemoryLock = smoochBot.MemoryLock;
+const Bot = smoochBot.Bot;
+
+describe('bot', function() {
+    let bot;
+    let store;
+    let lock;
+    let userId;
+
+    beforeEach(function() {
+        store = new MemoryStore();
+        lock = new MemoryLock();
+        userId = hat();
+        bot = new Bot();
+    });
+
+    describe('constructor', function() {
+        it('should create a bot', function() {
+            bot = new Bot({
+                store,
+                lock,
+                userIdss
+            });
+
+            bot.store.should.equal(store);
+            bot.lock.should.equal(lock);
+            bot.userId.should.equal(userId);
+        });
+
+        for (let omit of ['store', 'lock', 'userId']) {
+            it(`should fail if missing prop ${omit}`, function() {
+                let options = {
+                    store,
+                    lock,
+                    userId
+                };
+                delete options[omit];
+                expect(() => new Bot(options)).to.throw;
+            });
+        }
+    });
+
+    describe('method', function() {
+        beforeEach(function() {
+            bot = new Bot({
+                store,
+                lock,
+                userId
+            });
+        });
+
+        it('#say', function() {
+            return bot.say().should.be.fulfilled;
+        });
+    });
+});
