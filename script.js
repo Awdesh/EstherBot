7eed6472e24593536f092802758b09d6de46aea1
awdesh <awdesh.sharma1@gmail.com>
2016-05-26 19:59:38 -0700

    adding buttons inside json

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ module.exports = new Script({
     },
 
     speak: {
-        prompt: (bot) => bot.say("Anytime you can type Education, Career, Skill, Contact in order to learn about Awdesh"),
+        prompt: (bot) => bot.say("Anytime you can type Education, Career, Skill, Contact, LinkedIn in order to learn about Awdesh"),
         receive: (bot, message) => {
 
             let upperText = message.text.trim().toUpperCase();
