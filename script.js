60d581d65529e5fb7fe5f8a7bf7c37591c6be8a8
awdesh <awdesh.sharma1@gmail.com>
2016-05-26 21:23:31 -0700

    adding buttons inside json

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ module.exports = new Script({
     },
 
     speak: {
-        prompt: (bot) => bot.say('Anytime you can pick any of the below options-: %[Education]'),
+        prompt: (bot) => bot.say('Anytime you can pick any of the below options-: %[Education](postback:payload)'),
         receive: (bot, message) => {
 
             let upperText = message.text.trim().toUpperCase();
