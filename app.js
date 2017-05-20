8c14180cae0e64d8478b9a989dad742eb1fab9ba
awdesh <awdesh.sharma1@gmail.com>
2017-05-19 19:52:39 -0700

    updating view engine

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
 
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'html');
-app.use(express.static(__dirname + '/views'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
