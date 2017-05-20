5168cb2bae4cf8034dc56777b0d58832fb35ffc7
awdesh <awdesh.sharma1@gmail.com>
2017-05-19 19:34:31 -0700

    updating view engine

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const app = express();
 const path = require('path');
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
@@ -16,4 +15,3 @@ app.get('/', function(req, res) {
 });
 
 module.exports = app;
-
